ff67af0c660b980df3637eb6d6805a495118f408
MirandaLi1020 <lisirui19881020@gmail.com>
2022-07-21 01:30:03 +1000

    add style

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import '@aws-amplify/ui-react/styles.css';
 
 import Amplify from "aws-amplify";
 import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
