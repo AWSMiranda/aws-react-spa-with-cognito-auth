9ac030d39aeb608deb825cd014d07b2de52894fa
MirandaLi1020 <lisirui19881020@gmail.com>
2022-07-20 15:35:17 +1000

    dsfa

--- backend/bin/provisioning.ts
+++ backend/bin/provisioning.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "aws-cdk-lib/core";
+import * as cdk from "aws-cdk-lib";
 import { APIStack } from "../lib/api";
 import { AuthStack } from "../lib/auth";
 
--- frontend/provisioning/lib/frontend-stack.ts
+++ frontend/provisioning/lib/frontend-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from "aws-cdk-lib/core";
+import * as cdk from "aws-cdk-lib";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as s3 from "aws-cdk-lib/aws-s3";
